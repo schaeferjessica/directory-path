8d8d9eee3a096df7232088c7c52941470cc520e3
jschaefer <jessicaschaefer@hey.com>
2022-02-05 14:39:55 +0700

    feat(globals): add global css fornt size

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
     <div className="max-w-5lx mx-auto pl-9 pr-9">
       <nav className="flex justify-between mt-9">
         <header>
-          <h1 className="text-4xl">Directory Path</h1>
+          <h1>Directory Path</h1>
         </header>
         <ul className="flex">
           <li className="ml-4"><button className="border-midnight border-b-2">Dark Mode</button></li>
