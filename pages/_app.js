f2ece0b97a57f564576dc8f56d445b6c16384353
jschaefer <jessicaschaefer@hey.com>
2022-02-05 14:04:25 +0700

    feat: remove configuration options from tailwind.config

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,14 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="max-w-5lx mx-auto p-4">
-      <nav className="flex justify-between">
+    <div className="max-w-5lx mx-auto pl-9 pr-9">
+      <nav className="flex justify-between mt-9">
         <header>
-          <h1>Directory Path</h1>
+          <h1 className="text-4xl">Directory Path</h1>
         </header>
         <ul className="flex">
-          <li><button>Dark Mode</button></li>
-          <li><a href="https://github.com/schaeferjessica/directory-path">Github</a></li>
+          <li className="ml-4"><button className="border-midnight border-b-2">Dark Mode</button></li>
+          <li className="ml-4"><a className="border-midnight border-b-2 block" href="https://github.com/schaeferjessica/directory-path" target="_blank" rel="noreferrer">Github</a></li>
         </ul>
       </nav>
       
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 const Homepage = () => {
   return (
-    <div>
+    <div className="mt-20">
       <p>Tree JS</p>
       <p>Path</p>
     </div>
