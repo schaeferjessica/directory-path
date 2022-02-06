e5f20cc4cdeb8e9626dd53094f9606e7f961d497
jschaefer <jessicaschaefer@hey.com>
2022-02-06 17:15:44 +0700

    feat: add custom class for underline-text

--- pages/_app.js
+++ pages/_app.js
@@ -8,15 +8,15 @@ function MyApp({ Component, pageProps }) {
           <h1>Directory Path</h1>
         </header>
         <ul className="flex mt-2 sm:mt-0">
-          <li className="mr-3 sm:ml-4"><button className="linked">Dark Mode</button></li>
-          <li className="mr-3 sm:ml-4"><a className="linked block" href="https://github.com/schaeferjessica/directory-path" target="_blank" rel="noreferrer">Github</a></li>
+          <li className="mr-3 sm:ml-4"><button className="underline-text uppercase">Dark Mode</button></li>
+          <li className="mr-3 sm:ml-4"><a className="underline-text uppercase block" href="https://github.com/schaeferjessica/directory-path" target="_blank" rel="noreferrer">Github</a></li>
         </ul>
       </nav>
       
       <Component {...pageProps} />
 
       <footer className="mt-20 text-right">
-        <small>© 2022 UNIX PATH - build with ♥ and <a className="linked" href="https://nextjs.org/">Next.js</a></small>
+        <small>© 2022 UNIX PATH - build with ♥ and <a className="underline-text uppercase" href="https://nextjs.org/">Next.js</a></small>
       </footer>
     </div>
   )
