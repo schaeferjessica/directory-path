0f6822ad176dfc81b4211f1a5a3be24d13dab932
jschaefer <jessicaschaefer@hey.com>
2022-02-06 10:58:07 +0700

    feat: add custom class for underline-text

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
         'midnight': '#223956',
         'lavender': '#D3DAF2',
       },
+      keyframes: {
+        gradient: {
+          '0%': { transform: 'translate3d(0%, 0%, 0)' },
+          '100%': { transform: 'translate3d(-75%, 0%, 0)' },    
+          }      
+        }
     },  
     fontFamily: {
       sans: ['ui-sans-serif', 'sans-serif'],
