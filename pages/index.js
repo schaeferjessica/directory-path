f3b9321c921647635a713c1492ba2675ca94f216
thaisfinck <thaisfinck@gmail.com>
2022-02-07 15:24:14 +0700

    feat(index): Create tree component and add to index

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import dynamic from 'next/dynamic'
+import BasicTree from '../components/tree'
+
+const DynamicTree = dynamic(() => import('../components/tree'), {
+  ssr: false
+})
+
+
 const Homepage = () => {
   return (
     <div className="mt-20">
       <div>
         <small className="uppercase">Directory:</small>
+        <DynamicTree />
       </div>
       <div>
         <small className="uppercase">Relative Path:</small>
