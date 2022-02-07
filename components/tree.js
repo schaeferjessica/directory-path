c559e71131782a384ed9d73d36f9f7a5e20655ec
jschaefer <jessicaschaefer@hey.com>
2022-02-07 17:06:15 +0700

    feat(tree): add dynamic and import data.js file

--- components/tree.js
+++ components/tree.js
@@ -1,14 +1,17 @@
 
-import FolderTree, { testData } from 'react-folder-tree';
+import { testData } from '../pages/api/data';
+import dynamic from 'next/dynamic';
+
+const DynamicFileTreeImport = dynamic(() => import('react-folder-tree'), {
+  ssr: false, // needed to prevent warning about dynamic component
+});
 
 const BasicTree = () => {
   const onTreeStateChange = (state, event) => console.log(state, event);
+  console.log(testData);
 
   return (
-    <FolderTree
-      data={ testData }
-      onChange={ onTreeStateChange }
-    />
+    <DynamicFileTreeImport data={testData} onChange={onTreeStateChange} />
   );
 };
 
