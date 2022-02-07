503182c6b0d043efd980b2653e2f0b563e0053bd
jschaefer <jessicaschaefer@hey.com>
2022-02-07 16:42:44 +0700

    feat(tree): add component folder and add tree

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const About = () =>  {
     return (
       <div className="mt-20">
-        <p className="uppercase">In this page we are going to explain a litle about the project and how to find the path on our webapp.</p>
+        <p>In this page we are going to explain a litle about the project and how to find the path on our webapp.</p>
     
         <div className="uppercase underline-text mt-4">
           <Link href="/">Back to home</Link>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
+import BasicTree from "../components/tree";
+
 const Homepage = () => {
   return (
     <div className="mt-20">
       <div>
         <small className="uppercase">Directory:</small>
+        <BasicTree />
       </div>
       <div>
         <small className="uppercase">Relative Path:</small>
