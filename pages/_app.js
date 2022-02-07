7e8df20d53947b2d85ad2f2f8828b8308504e9af
thaisfinck <thaisfinck@gmail.com>
2022-02-07 11:37:38 +0700

    feat(_app): Add class to the about link

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
             <h1>Directory Path</h1>
           </header>
           <ul className="flex mt-2 sm:mt-0">
-            <li><Link href="/about" className="underline-text uppercase">About</Link></li>
+            <li className="mr-3 sm:ml-4 underline-text uppercase"><Link href="/about">About</Link></li>
             <li className="mr-3 sm:ml-4"><button className="underline-text uppercase">Dark Mode</button></li>
             <li className="mr-3 sm:ml-4"><a className="underline-text uppercase block" href="https://github.com/schaeferjessica/directory-path" target="_blank" rel="noreferrer">Github</a></li>
           </ul>
