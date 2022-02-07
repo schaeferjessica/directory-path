57403bfd10a9744faf3d1aac8c142b20507a616e
jschaefer <jessicaschaefer@hey.com>
2022-02-07 16:44:49 +0700

    feat(_app): remove next Link

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,6 @@ function MyApp({ Component, pageProps }) {
             <h1><Link href="/">Directory Path</Link></h1>
           </header>
           <ul className="flex mt-2 sm:mt-0">
-            <li className="mr-3 sm:ml-4 underline-text uppercase"><Link href="/tree">Tree</Link></li>
             <li className="mr-3 sm:ml-4 underline-text uppercase"><Link href="/about">About</Link></li>
             <li className="mr-3 sm:ml-4"><button className="underline-text uppercase">Dark Mode</button></li>
             <li className="mr-3 sm:ml-4"><a className="underline-text uppercase block" href="https://github.com/schaeferjessica/directory-path" target="_blank" rel="noreferrer">Github</a></li>
