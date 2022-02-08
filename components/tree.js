9068d98a1fec7e7898f0fc3c5510b79ee49b61a2
thaisfinck <thaisfinck@gmail.com>
2022-02-08 14:13:33 +0700

    feat(tree): trying a new method

--- components/tree.js
+++ components/tree.js
@@ -7,11 +7,15 @@ const DynamicFileTreeImport = dynamic(() => import('react-folder-tree'), {
 
 const BasicTree = () => {
   const onTreeStateChange = (state, event) => {
-    if (event.type == 'checkNode') {
+    if (state.checked == '1') {  
+      //  state.children.forEach((child, i) => {
+      //  console.log(state.children[i])
+      //  })
       console.log(state.children)
+    }
       console.log(state, event);
-      console.log(testData); }
-  }
+      console.log(testData); 
+}
   return (
     <DynamicFileTreeImport data={testData} onChange={onTreeStateChange} />
   );
