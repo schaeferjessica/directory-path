5e008d51e3e756b50e2d690052c16de164660e4e
jschaefer <jessicaschaefer@hey.com>
2022-02-09 12:59:39 +0700

    feat(404): add 404 page

---
+++ pages/404.js
@@ -0,0 +1,3 @@
+export default function Custom404() {
+  return <h1 className="mono flex justify-center items-center h-screen">404 - Page Not Found</h1>
+}
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-       <Head>
+      <Head>
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300&family=IBM+Plex+Sans:wght@300&display=swap" rel="stylesheet" />             
       </Head>
       <body>
