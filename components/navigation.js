bf769e5e1c58911daad852c0af5f5a990d79b934
jschaefer <jessicaschaefer@hey.com>
2022-02-11 09:52:01 +0700

    feat(navigation): comment out dark mode

--- components/navigation.js
+++ components/navigation.js
@@ -8,7 +8,7 @@ export default function Navigation() {
       <h1><Link href="/">Directory Path</Link></h1>
       <ul className="flex mt-2 sm:mt-0">
         <li className="mr-3 sm:ml-4 underline-text uppercase"><Link href="/about">About</Link></li>
-        <li className="mr-3 sm:ml-4"><button className="underline-text uppercase">Dark Mode</button></li>
+        {/* <li className="mr-3 sm:ml-4"><button className="underline-text uppercase">Dark Mode</button></li> */}
         <li className="mr-3 sm:ml-4"><a className="underline-text uppercase block" href="https://github.com/schaeferjessica/directory-path" target="_blank" rel="noreferrer">Github</a></li>
       </ul>
     </nav>
