c9dedecd5bed4f92d8ea5d02ea8fe3e798633633
jschaefer <jessicaschaefer@hey.com>
2022-02-17 11:07:22 +0700

    feat(about): change about to how-to-use

--- components/navigation.js
+++ components/navigation.js
@@ -7,7 +7,7 @@ export default function Navigation() {
     <nav className="sm:flex sm:justify-between sm:items-center mt-4 sm:mt-6">
       <h1><Link href="/">Directory Path</Link></h1>
       <ul className="flex mt-2 sm:mt-0">
-        <li className="mr-3 sm:ml-4 underline-text uppercase"><Link href="/about">About</Link></li>
+        <li className="mr-3 sm:ml-4 underline-text uppercase"><Link href="/how-to-use">How to Use</Link></li>
         {/* <li className="mr-3 sm:ml-4"><button className="underline-text uppercase">Dark Mode</button></li> */}
         <li className="mr-3 sm:ml-4"><a className="underline-text uppercase block" href="https://github.com/schaeferjessica/directory-path" target="_blank" rel="noreferrer">Github</a></li>
       </ul>
---
+++ pages/how-to-use.js
@@ -0,0 +1,57 @@
+import Layout from '../components/layout'
+import Head from 'next/head'
+import Container from '../components/container'
+import Link from 'next/link'
+
+export default function About() {
+  return (
+    <Layout>
+      <Head>
+        <title>Directory Path - How to Use</title>
+      </Head>
+      <Container>
+        <div className="mt-12">
+          <div>
+            <h2 className='uppercase mb-3'>get startet</h2>
+            <p>how to find your path via Directory Path Generator:</p>
+            <br />
+            <span>click the box</span>
+
+            <div className="CheckBox ml-2 mr-2">
+              <input className="checkboxDOM" type="checkbox"></input>
+              <span className="checkbox-span"></span>
+            </div>
+
+            <span>to select the <strong className='uppercase font-normal'>file</strong> you would like the path going <strong className='uppercase font-normal'>from</strong></span>
+            <br />
+            <span>then click the secound box</span>
+
+            <div className="CheckBox ml-2 mr-2">
+              <input className="checkboxDOM" type="checkbox"></input>
+              <span className="checkbox-span"></span>
+            </div>
+
+            <span>to select the <strong className='uppercase font-normal'>file</strong> you would like the path going <strong className='uppercase font-normal'>to</strong></span>
+            <p className='mt-3'>the RELATIVE PATH will be generate for you :)</p>
+          </div>
+         
+
+          <div className='mt-8'>
+            <h2 className='uppercase mb-3'>the tool</h2>
+            <p>directory path is a developer tool to help developers to get the path to reach from one file to another.</p> 
+          </div>
+
+          <div className='mt-8'>
+            <h2 className='uppercase mb-3'>reach out</h2>
+            <p>email@email.com</p>
+            <p>We are happy to hear from you!</p> 
+          </div>
+
+          <div className="uppercase underline-text mt-8">
+            <Link href="/">generate a path</Link>
+          </div>
+        </div>
+      </Container>
+    </Layout>
+  )
+}
